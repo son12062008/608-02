0aff381bd45ffe9f1868773d3840c7964dafe15a
son12062008 <nooson1206@gmail.com>
2026-06-17 15:31:49 +0700

    Add files via upload

---
+++ ใบงานที่3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>ข้อมูลนักเรียน</title>
+</head>
+<body>
+
+<?php
+echo "<font color='blue'>ชื่อ-สกุล : ปภังกร ลาภาศุภวัฒน์</font><br>";
+echo "<font color='green'>เรียนอยู่ระดับชั้น : ม.6 ห้อง 8</font><br>";
+echo "<font color='red'>เลขที่ : 2</font><br>";
+echo "<font color='purple'>ที่อยู่ : กรุงเทพมหานคร</font><br>";
+echo "<font color='orange'>ประวัติการศึกษา : จบมัธยมศึกษาตอนต้น</font><br>";
+?>
+
+</body>
+</html>
